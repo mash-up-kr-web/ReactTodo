a4f4bc71d9c24318c67e71f9395ff9f2da6b481b
Yuni-Q <carnotcycle2@naver.com>
2019-06-09 16:32:22 +0900

    temp

--- src/Signin.js
+++ src/Signin.js
@@ -1,57 +1,70 @@
-import axios from 'axios';
-import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
+import axios from "axios";
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
 
-import { url } from './const';
+import { url } from "./const";
 
 class Signin extends Component {
   state = {
-    email: '',
-    password: '',
-    error: '',
-  }
+    email: "",
+    password: "",
+    error: ""
+  };
 
-  onChange = (e) => {
+  onChange = e => {
     this.setState({
-      [e.target.name]: e.target.value,
-    })
-  }
+      [e.target.name]: e.target.value
+    });
+  };
 
-  onSubmit = async (e) => {
-    const { email, password } = this.state
-    e.preventDefault()
+  onSubmit = async e => {
+    const { email, password } = this.state;
+    e.preventDefault();
     const result = await axios.post(`${url}/signIn`, {
       email,
-      password,
-    })
-    if ( result.status === 200 ) {
-      window.localStorage.setItem('token', result.data.result.token);
-      this.props.history.push('/');
+      password
+    });
+    if (result.status === 200) {
+      window.localStorage.setItem("token", result.data.result.token);
+      this.props.history.push("/");
     } else {
       this.setState({
-        error: result.data.error,
-      })
+        error: result.data.error
+      });
     }
-  }
-  
+  };
+
   render() {
-    const { email, password, error } = this.state
+    console.log("props", this.props);
+    const { email, password, error } = this.state;
     return (
       <>
         <h1>로그인</h1>
-        {!!error && <div style={{color: "red"}}>{error}</div>}
+        {!!error && <div style={{ color: "red" }}>{error}</div>}
         <form onSubmit={this.onSubmit}>
-          <label htmlFor='email'>email</label>
+          <label htmlFor="email">email</label>
           <br />
-          <input type="text" name="email" id="email" value={email} onChange={this.onChange} />
+          <input
+            type="text"
+            name="email"
+            id="email"
+            value={email}
+            onChange={this.onChange}
+          />
           <br />
           <label htmlFor="password">password</label>
           <br />
-          <input type="password" name="password" id="password" value={password} onChange={this.onChange} />
+          <input
+            type="password"
+            name="password"
+            id="password"
+            value={password}
+            onChange={this.onChange}
+          />
           <br />
           <button>로그인</button>
         </form>
-        <Link to='signup'>회원가입</Link>
+        <Link to="signup">회원가입</Link>
       </>
     );
   }
--- src/Todos.js
+++ src/Todos.js
@@ -1,48 +1,44 @@
-import axios from 'axios';
-import React, { Component } from 'react';
-import { Redirect } from 'react-router-dom';
+import axios from "axios";
+import React, { Component } from "react";
+import { Redirect } from "react-router-dom";
 
-const url = 'http://13.113.246.46:8081';
+const url = "http://13.113.246.46:8081";
 
 class Todos extends Component {
   state = {
-    token: window.localStorage.getItem('token'),
-    list: [],
-  }
-  async componentDidMount () {
+    redirect: false,
+    token: window.localStorage.getItem("token"),
+    list: []
+  };
+  async componentDidMount() {
     const result = await axios.get(`${url}/todos`, {
       headers: {
-        Authorization: 'Bearer ' + this.state.token //the token is a variable which holds the token
+        Authorization: "Bearer " + this.state.token //the token is a variable which holds the token
       }
     });
-    if(result.status === 200 && result.data.ok === true) {
+    if (result.status === 200 && result.data.ok === true) {
       this.setState({
-        list: result.data.result,
-      })
+        list: result.data.result
+      });
     } else {
       console.log(result.data.error);
     }
-    
   }
   render() {
     const { token } = this.state;
     console.log(this.state.list);
     return (
       <>
-        {!token && <Redirect to='/signin' />}
+        {!token && <Redirect to="/signin" />}
         <table border="1">
           <thead>
             <tr>
-              <td>
-                안녕
-              </td>
+              <td>안녕</td>
             </tr>
           </thead>
           <tbody>
             <tr>
-              <td>
-                하하
-              </td>
+              <td>하하</td>
             </tr>
           </tbody>
         </table>
